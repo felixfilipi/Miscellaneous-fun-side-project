510d0804fc51302ab03fde62f56bc4ec398dd5be
Felix Filipi <65327805+felixfilipi@users.noreply.github.com>
2021-11-08 20:40:49 +0700

    Dc Preence

--- Discord_Presence.py
+++ Discord_Presence.py
@@ -1,22 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import psutil
 from pypresence import Presence
 import time
 
 client_id = '#'  # Fake ID, put your real one here
-RPC = Presence(client_id,pipe=0)  # Initialize the client class
-RPC.connect() # Start the handshake loop
+RPC = Presence(client_id,pipe=0)  
+RPC.connect() 
 
 
 while True:  # The presence will stay on as long as the program is running
-    cpu_per = round(psutil.cpu_percent(),1) # Get CPU Usage
+    cpu_per = round(psutil.cpu_percent(),1) 
     mem = psutil.virtual_memory()
-    mem_per = round(psutil.virtual_memory().percent,1)# Set the presence
-    print(RPC.update(details="Brain performance: "+str(mem_per)+"%", state="Insanity: "+str(cpu_per)+"%"+"\n + Sumpek"))  # Set the presence
-    time.sleep(60) # Can only update rich presence every 15 seconds
-
+    mem_per = round(psutil.virtual_memory().percent,1)
+    print(RPC.update(details="Brain performance: "+str(mem_per)+"%", state="Insanity: "+str(cpu_per)+"%"+"\n + Sumpek"))  
+    time.sleep(60) 
